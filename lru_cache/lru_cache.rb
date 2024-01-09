d8fd586947e4ecb09f8217f5a16e1c36072a94e3
Charles Bandes <charles.bandes@modolabs.com>
2024-01-09 11:41:48 -0500

    Solution - Charley
    This is my solution for the LRUCache problem. I might have
    overcomplicated things by adding a second class for the cache records,
    maybe a Struct would have been more appropriate, idk. I hadn't known
    about `min_by` which is pretty cool and seems to work well. Tests are
    green, so I think this works.
    
    I did add some entries to gitignore to make sure that we weren't
    committing our bundles. I moved the spec so that I could run rspec more
    easily, and I added a couple of tests of my own during the development
    process.

--- lru_cache/lru_cache.rb
+++ lru_cache/lru_cache.rb
@@ -1,12 +1,38 @@
 class LRUCache
+  attr_reader :capacity
+  attr_accessor :cache
+
+  class CacheItem
+    attr_reader :key, :value, :created_at
+
+    def initialize(key:, value:)
+      @key = key
+      @value = value
+      @created_at = Time.new.to_f
+    end
+  end
 
   def initialize(capacity)
+    @capacity = capacity
+    @cache = {}
   end
 
   def get(key)
+    cache[key]&.value || -1
   end
 
   def put(key, value)
+    evict if ((cache.count >= capacity) && cache[key].nil?)
+    cache[key] = CacheItem.new(key:, value:)
   end
 
+  private
+
+  def evictee
+    cache.min_by{ |_key, item| item.created_at }[1]
+  end
+
+  def evict
+    cache.reject!{ |_key, item| item == evictee }
+  end
 end
--- lru_cache/spec/lru_cache_spec.rb
+++ lru_cache/spec/lru_cache_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative "lru_cache.rb"
+require "./lru_cache.rb"
 
 describe LRUCache do
   it 'README example' do
@@ -35,5 +35,34 @@ describe LRUCache do
     expect(lru_cache.get(1)).to eq 11
     expect(lru_cache.get(2)).to eq -1
     expect(lru_cache.get(3)).to eq 3
+  end
+
+  it 'puts one value' do
+    lru_cache = LRUCache.new 1
+    lru_cache.put(1, 'something')
+  end
+
+  it 'evicts oldest value' do
+    lru_cache = LRUCache.new 1
+    lru_cache.put(1, 'something')
+    expect(lru_cache.get(1)).to eq 'something'
+    lru_cache.put(5, 'something_else')
+    expect(lru_cache.get(1)).to eq -1
+  end
+
+  it 'works with capacity 3' do
+    lru_cache = LRUCache.new 3
+    lru_cache.put(1, 'something')
+    expect(lru_cache.get(1)).to eq 'something'
+    lru_cache.put(5, 'something_else')
+    lru_cache.put(3, 'a third thing')
+    expect(lru_cache.get(1)).to eq 'something'
+    expect(lru_cache.get(5)).to eq 'something_else'
+    expect(lru_cache.get(3)).to eq 'a third thing'
+    lru_cache.put(77, 925)
+    expect(lru_cache.get(1)).to eq -1
+    expect(lru_cache.get(5)).to eq 'something_else'
+    expect(lru_cache.get(3)).to eq 'a third thing'
+    expect(lru_cache.get(77)).to eq 925
   end
 end
