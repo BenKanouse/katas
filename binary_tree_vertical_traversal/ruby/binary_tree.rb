87a4438b90a090c01bbcc070d3834ca29fd7008f
Ben Kanouse <kanobt61@gmail.com>
2024-03-01 12:27:30 -0500

    Binary tree solution

--- binary_tree_vertical_traversal/ruby/binary_tree.rb
+++ binary_tree_vertical_traversal/ruby/binary_tree.rb
@@ -2,10 +2,48 @@ class BinaryTree
 
   def initialize(root_node)
     @root_node = root_node
+    @data = Hash.new([])
   end
 
   def vertical_traversal
-    # TODO
+    paths root_node, 0, 0
+
+    data.sort.each_with_object(Hash.new([])) do |(point, value), hash|
+      hash[point.x] = hash[point.x] + value
+    end.values
   end
 
+  private
+
+  attr_reader :data, :root_node
+
+  def paths(node, x, y)
+    return unless node
+
+    point = Point.new(x, y)
+    data[point] = data[point] + [node.value]
+
+    paths(node.left, x - 1, y + 1)
+    paths(node.right, x + 1, y + 1)
+  end
+
+end
+
+class Point
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def ==(other)
+    x == other.x && y == other.y
+  end
+
+  def <=>(other)
+    return y <=> other.y if x == other.x
+
+    x <=> other.x
+  end
 end
