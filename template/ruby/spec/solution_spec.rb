c46febe69ebc34babb8a23c0712d14369bb6d4a7
Ben Kanouse <kanobt61@gmail.com>
2024-07-17 11:42:44 -0400

    Run rubocop on the template folder
    Rubocop fixed a few small style issues in the template.

--- template/ruby/spec/solution_spec.rb
+++ template/ruby/spec/solution_spec.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require './solution'
 
 describe Solution do
-
   it 'returns nil' do
     expect(described_class.new.run).to eq nil
   end
-
 end
